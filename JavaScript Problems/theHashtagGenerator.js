b6dc4e4e21222db3c3c3fb2991ea650f81362f65
Tivon <badboytjy@gmail.com>
2022-09-24 13:24:16 -0700

    Algos

---
+++ JavaScript Problems/theHashtagGenerator.js
@@ -0,0 +1,34 @@
+
+
+//  The marketing team is spending way too much time typing in hashtags.
+// Let's help them with our own Hashtag Generator!
+
+// Here's the deal:
+
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+// If the final result is longer than 140 chars it must return false.
+// If the input or the result is an empty string it must return false.
+// Examples
+// " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+// "    Hello     World   "                  =>  "#HelloWorld"
+// ""                                        =>  false
+
+
+function generateHashtag(str){
+
+    let newStr = str
+    .toLowerCase()
+    .split(" ")
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join("");
+
+    if(str.length == 0 || newStr >= 140 || str.search(/[\w]/g)){
+        return false;
+    }
+
+    return "#" + newStr
+
+}
+
+console.log(generateHashtag("hi im tivon"))
