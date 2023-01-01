3e162e33daacdb7db677f5f78f674bed12a97e6d
Tivon Yoo <90356905+Voltivon@users.noreply.github.com>
2022-12-31 20:57:39 -0800

    Create sliceAndSplice.js

---
+++ JavaScript Problems/sliceAndSplice.js
@@ -0,0 +1,14 @@
+function frankenSplice(arr1, arr2, n) {
+  let localArr = arr2.slice()
+
+  for(let i = 0; i < arr1.length; i++){
+    localArr.splice(n, 0, arr1[i])
+    n++;
+  }
+
+  return localArr
+}
+
+
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
