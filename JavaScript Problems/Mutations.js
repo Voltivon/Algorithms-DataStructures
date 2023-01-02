44653175b69c855654d768e5b2d89760474c3b02
Tivon Yoo <90356905+Voltivon@users.noreply.github.com>
2023-01-01 17:46:38 -0800

    Create Mutations.js

---
+++ JavaScript Problems/Mutations.js
@@ -0,0 +1,14 @@
+function mutation(arr) {
+  
+  const firstArr = arr[0].toLowerCase()
+  const secondArr = arr[1].toLowerCase()
+  
+  for(let i = 0; i < secondArr.length; i++){
+    if(firstArr.indexOf(secondArr[i]) < 0) return false
+  }
+  return true
+
+ 
+}
+
+mutation(["hello", "Hello"]);
