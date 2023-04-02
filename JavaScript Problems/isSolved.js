f8885bd4bb35ded41e5d23abc4280c4153b04d8b
Tivon <badboytjy@gmail.com>
2023-04-01 23:06:12 -0700

    Check state of tic tac toe game

--- JavaScript Problems/isSolved.js
+++ JavaScript Problems/isSolved.js
@@ -1,42 +1,52 @@
 function isSolved(board) {
-    // TODO: Check if the board is solved!
-    
+  // TODO: Check if the board is solved!
+
   //Check rows
-    
-    for(let i = 0; i < 3; i++){
-      if(board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] !== 0){
-        return board[i][0];
-        
-      }
-      }
-  //     Check Columns
-    for(let i = 0; i < 3; i++){
-      if(board[0][i] == board[1][i] &&  board[0][i] == board[2][i] && board[0][i] !== 0){
-        return board[0][i];
-      }
+
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[i][0] == board[i][1] &&
+      board[i][0] == board[i][2] &&
+      board[i][0] !== 0
+    ) {
+      return board[i][0];
     }
-    
-  //   Check diagonals
-    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] && board !== 0){
-      return board[0][0];
-    }
-      
-    if(board[0][2] == board[1][1] && board[0][2] == board[2][0] && board !== 0){
-      return board[0][2];
-    }
-      
-      for(let i = 0; i < 3; i++){
-        for(let j = 0; j < 3; j++){
-          if(board[i][j] == 0){
-            return -1;
-          }
-        }
-      }
-      
-      return 0;
-      
-      
-      
-    
-    
   }
+  //     Check Columns
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[0][i] == board[1][i] &&
+      board[0][i] == board[2][i] &&
+      board[0][i] !== 0
+    ) {
+      return board[0][i];
+    }
+  }
+
+  //   Check diagonals
+  if (
+    board[0][0] == board[1][1] &&
+    board[0][0] == board[2][2] &&
+    board[0][0] !== 0
+  ) {
+    return board[0][0];
+  }
+
+  if (
+    board[0][2] == board[1][1] &&
+    board[0][2] == board[2][0] &&
+    board[0][2] !== 0
+  ) {
+    return board[0][2];
+  }
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] === 0) {
+        return -1;
+      }
+    }
+  }
+
+  return 0;
+}
