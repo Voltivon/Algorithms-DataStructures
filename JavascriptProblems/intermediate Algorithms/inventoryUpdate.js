160032a2787e580408d0cb192ae0e48e16eb9e2e
Tivon <badboytjy@gmail.com>
2023-07-01 23:10:53 -0700

    Completed

---
+++ JavascriptProblems/intermediate Algorithms/inventoryUpdate.js
@@ -0,0 +1,54 @@
+/*
+Inventory Update
+
+Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. 
+Update the current existing inventory item quantities (in arr1).
+If an item cannot be found, add the new item and quantity into the inventory array. 
+The returned inventory array should be in alphabetical order by item.
+
+*/
+
+function updateInventory(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i][1] === arr2[j][1]) {
+        arr1[i][0] += arr2[j][0];
+        break;
+      }
+    }
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    let found = false;
+    for (let j = 0; j < arr1.length; j++) {
+      if (arr2[i][1] === arr1[j][1]) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      arr1.push(arr2[i]);
+    }
+  }
+  arr1.sort((a, b) => a[1].localeCompare(b[1]));
+
+  return arr1;
+}
+
+// Example inventory lists
+var curInv = [
+  [21, "Bowling Ball"],
+  [2, "Dirty Sock"],
+  [1, "Hair Pin"],
+  [5, "Microphone"],
+];
+
+var newInv = [
+  [2, "Hair Pin"],
+  [3, "Half-Eaten Apple"],
+  [67, "Bowling Ball"],
+  [7, "Toothpaste"],
+];
+
+console.log(updateInventory(curInv, newInv));
